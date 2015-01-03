f0db7c900bd8baa13304969e6d9c82b8065100c6
Melissa McWilliams <mmcwilliams@hidglobal.com>
2015-01-03 16:47:06 -0600

    modifying IDs for product solutions

--- www/js/services.js
+++ www/js/services.js
@@ -3,15 +3,14 @@ angular.module('starter.services', [])
 /**
  * A simple example service that returns some data.
  */
-.factory('Friends', function() {
+.factory('Products', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
   var friends = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
+    { id: 0, name: 'Physical Access' },
+    { id: 1, name: 'Logical Access' },
+    { id: 2, name: 'Card Printers' }
   ];
 
   return {
