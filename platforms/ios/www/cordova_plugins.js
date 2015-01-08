db6d6aab29fdbd08b69665130b9029340a3bb5b8
Melissa McWilliams <mmcwilliams@hidglobal.com>
2015-01-08 10:26:04 -0600

    changes to config and android build

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -28,13 +28,6 @@ module.exports = [
         "clobbers": [
             "datePicker"
         ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
-        "id": "org.apache.cordova.splashscreen.SplashScreen",
-        "clobbers": [
-            "navigator.splashscreen"
-        ]
     }
 ];
 module.exports.metadata = 
@@ -43,8 +36,7 @@ module.exports.metadata =
     "de.appplant.cordova.plugin.email-composer": "0.8.2dev",
     "nl.x-services.plugins.socialsharing": "4.3.8",
     "org.apache.cordova.inappbrowser": "0.5.3",
-    "com.plugin.datepicker": "0.4.0",
-    "org.apache.cordova.splashscreen": "0.3.5"
+    "com.plugin.datepicker": "0.4.0"
 }
 // BOTTOM OF METADATA
 });
