a2ff4359821a19e41cdb614079ddb3e5cde123bd
Melissa McWilliams <mmcwilliams@hidglobal.com>
2015-01-14 11:42:58 -0600

    adding updated SUCCESSFUL builds

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -8,20 +8,6 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
-        "id": "org.apache.cordova.inappbrowser.inappbrowser",
-        "clobbers": [
-            "window.open"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
-        "id": "org.apache.cordova.splashscreen.SplashScreen",
-        "clobbers": [
-            "navigator.splashscreen"
-        ]
-    },
-    {
         "file": "plugins/de.appplant.cordova.plugin.email-composer/www/email_composer.js",
         "id": "de.appplant.cordova.plugin.email-composer.EmailComposer",
         "clobbers": [
@@ -35,16 +21,30 @@ module.exports = [
         "clobbers": [
             "window.plugins.socialsharing"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "com.plugin.datepicker": "0.4.0",
-    "org.apache.cordova.inappbrowser": "0.5.3",
-    "org.apache.cordova.splashscreen": "0.3.5",
     "de.appplant.cordova.plugin.email-composer": "0.8.2dev",
-    "nl.x-services.plugins.socialsharing": "4.3.12"
+    "nl.x-services.plugins.socialsharing": "4.3.12",
+    "org.apache.cordova.inappbrowser": "0.5.3",
+    "org.apache.cordova.splashscreen": "0.3.5"
 }
 // BOTTOM OF METADATA
 });
--- platforms/ios/HID Product Solutions/main.m
+++ platforms/ios/HID Product Solutions/main.m
@@ -18,7 +18,7 @@
  */
 //
 //  main.m
-//  HID Product Solutions
+//  Restaurant
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
